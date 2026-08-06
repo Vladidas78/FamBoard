b331bce4b7c2f4c113a369bd7568ed1e4401c177
Vladidas78 <flavor7878@gmail.com>
2026-08-06 20:14:00 +0200

    Add files via upload
    visuel update

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 /* FamBoard – Service Worker
    Shell wird gecacht, damit die App auch ohne Netz startet.
    Die Daten selbst kommen von Firebase und werden dort nie gecacht. */
-const CACHE = 'famboard-v15';
+const CACHE = 'famboard-v16';
 const SHELL = [
   './',
   './index.html',
